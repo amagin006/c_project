8634f30157e88b89c286a8a8d358f21bfe79811a
Shota Iwamoto <amaginqoo@gmail.com>
2019-02-22 16:19:33 -0800

    Create test-case

---
+++ test_vc_isdigit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void test_vc_isdigit() {
+
+   
+    printf("==========isdigit=============\n");
+    printf("========== expect : 1 =============\n");
+    printf("--- :%d \n",  vc_isdigit(9));
+
+    printf("==========isdigit=============\n");
+    printf("========== expect : 0 =============\n");
+    printf("--- :%d \n",  vc_isdigit(200));
+
+}
+
+
---
+++ test_vc_itoa.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void test_vc_itoa() {
+  
+    char *str = vc_itoa(1390);
+    printf("==========vc_itoa=============\n");
+    printf("========== expect : 1390  =============\n");
+    printf("--- :%s \n", str);
+}
+
+
