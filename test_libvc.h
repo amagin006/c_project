b00a5bb40dd9dab3ccdd327a835abae0e2d57c2c
kat2n <katsumata.coffee.150yen@gmail.com>
2019-02-22 16:26:58 -0800

    add test_libvc.h

---
+++ test_libvc.h
@@ -0,0 +1,137 @@
+#ifndef TEST_LIBVC_H
+#define TEST_LIBVC_H
+#include <stdio.h>
+
+// test_vc_atoi
+void test_vc_atoi();
+
+// test_vc_bzero
+void test_vc_bzero();
+
+// test_vc_isalnum
+void test_vc_isalnum();
+
+// test_vc_isalpha
+void test_vc_isalpha();
+
+// test_vc_isascii
+void test_vc_isascii();
+
+// test_vc_isdigit
+void test_vc_isdigit();
+
+// test_vc_isprint
+void test_vc_isprint();
+
+// test_vc_itoa
+void test_vc_itoa();
+
+// test_vc_memalloc
+void test_vc_memalloc();
+
+// test_vc_memccpy
+void test_vc_memccpy();
+
+// test_vc_memchr
+void test_vc_memchr();
+
+// test_vc_memcmp
+void test_vc_memcmp();
+
+// test_vc_memcpy
+void test_vc_memcpy();
+
+// test_vc_memdel
+void test_vc_memdel();
+
+// test_vc_memmove
+void test_vc_memmove();
+
+// test_vc_memset
+void test_vc_memset();
+
+// test_vc_putchar
+void test_vc_putchar();
+
+// test_vc_putendl
+void test_vc_putendl();
+
+// test_vc_putnbr
+void printnumber();
+
+// test_vc_puts()
+void test_vc_putstr();
+
+// test_vc_strcat
+void test_vc_strcat();
+
+// test_vc_strchr
+void test_vc_strchr();
+
+// test_vc_strclr
+void test_vc_strclr();
+
+// test_vc_strcmp
+void test_vc_strcmp();
+
+// test_vc_strcpy
+void test_vc_strcpy();
+
+// test_vc_strdel
+void test_vc_strdel();
+
+// test_vc_strdup
+void test_vc_strdup();
+
+// test_vc_striter
+void test_vc_striter();
+
+// test_vc_strjoin
+void test_vc_strjoin();
+
+// test_vc_strlcat
+void test_vc_strlcat();
+
+// test_vc_strlen
+void test_vc_strlen();
+
+// test_vc_strmap
+void test_vc_strmap();
+
+// test_vc_strncat
+void test_vc_strncat();
+
+// test_vc_strncmp
+void test_vc_strncmp();
+
+// test_vc_strncpy
+void test_vc_strncpy();
+
+// test_vc_strnew
+void test_vc_strnew();
+
+// test_vc_strnstr
+void test_vc_strnstr();
+
+// test_vc_strrchr
+void test_vc_strrchr();
+
+// test_vc_strsplit
+void test_vc_strsplit();
+
+// test_vc_strstr
+void test_vc_strstr();
+
+// test_vc_strsub
+void test_vc_strsub();
+
+// test_vc_strtrim
+void test_vc_strtrim();
+
+// test_vc_tolower
+void test_vc_tolower();
+
+// test_vc_toupper
+void test_vc_toupper();
+
+#endif //TEST_LIBVC_H
